b8e8bab2bde2f03a2cbb1ee2e88dba291686f3e5
Harizmudrik <muhammadharizmudrik@gmail.com>
2023-06-19 11:16:40 +0700

    menambahkan catch int a untuk blok yang akan dieksekusi

--- manualExeption_141/manualExeption_141.cpp
+++ manualExeption_141/manualExeption_141.cpp
@@ -10,8 +10,13 @@ int main()
     try {
         cout << "Selamat Belajar di Prodi TI UMY" << endl;
         //throw 0.5; //melemparkan sebuah integer maka
-
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
+    catch (int a) {
+        //blok ini akan dieksekusi 
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
