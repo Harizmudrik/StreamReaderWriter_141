2e516e84d4b6259dcb765971fec04de9c116c33f
Harizmudrik <muhammadharizmudrik@gmail.com>
2023-06-19 11:09:23 +0700

    Membuat perintah yang sama dengan ioFile

--- iofileWithPath_141/iofileWithPath_141.cpp
+++ iofileWithPath_141/iofileWithPath_141.cpp
@@ -15,7 +15,52 @@ int main()
 
     cout << "Masukkan Nama File : ";
     cin >> NamaFile;
+
+    //membuka file dalam mode menulis.
+    ofstream outfile;
+    //menunjuk ke sebuah nama file 
+    outfile.open(NamaFile, ios::out);
+
+    cout << ">=  Menulis file, \'q\' untuk keluar" << endl;
+
+    //Unlimited loop untuk menulis 
+    while (true) {
+        cout << "_ ";
+        //mendapatkan setiap karakter dalam satu baris 
+        getline(cin, baris);
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam file 
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup file nya 
+    outfile.close();
+
+    //membuka file dalam mode membaca 
+    ifstream infile;
+    //menunjuk ke sebuah file 
+    infile.open(NamaFile, ios::in);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka 
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris 
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini 
+            cout << baris << '\n';
+
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini 
+    else cout << "Unable to open file";
+    return 0;
 }
+
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
