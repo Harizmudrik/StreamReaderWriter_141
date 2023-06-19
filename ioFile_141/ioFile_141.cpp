06647e224a8383dd0312946930ae9db0a6bda15c
Harizmudrik <muhammadharizmudrik@gmail.com>
2023-06-19 10:35:04 +0700

    Menambahkan unutk menunjuk ke sebuah file dalam mode baca

--- ioFile_141/ioFile_141.cpp
+++ ioFile_141/ioFile_141.cpp
@@ -31,6 +31,14 @@ int main()
 	//selesai dalam menulis sekarang tutup filenya 
 	outfile.close(); 
 
+	//membuka file dalam mode membaca 
+	ifstream infile;
+	//menunjuk ke sebuah file 
+	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+
+
 
 }
 
