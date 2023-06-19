2fb5690d0a9c7db763929324d29e1bbdbc4a6132
Harizmudrik <muhammadharizmudrik@gmail.com>
2023-06-19 10:29:46 +0700

    Menambahkan arahan >= Menulis file, \'q\; untuk keluar"

--- ioFile_141/ioFile_141.cpp
+++ ioFile_141/ioFile_141.cpp
@@ -3,6 +3,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
 
 
@@ -10,6 +11,14 @@ using namespace std;
 int main()
 {
 	string baris; 
+	//membuka file dalam mode menulis.
+	ofstream outfile;
+	// menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\; untuk keluar" << endl; 
+
+
 
 
 }
